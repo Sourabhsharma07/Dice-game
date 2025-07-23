26806f612ade1fb7e95fce885f840f98c6f46750
Sourabhsharma07 <151004812+Sourabhsharma07@users.noreply.github.com>
2025-07-23 21:19:38 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,25 @@
+package dice_game;
+
+
+public class Player {
+    private String name;
+    private int wins;
+
+    public Player(String name) {
+        this.name = name;
+        this.wins = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void incrementWins() {
+        wins++;
+    }
+}
 
